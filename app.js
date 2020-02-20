3c513717a300c4883bbdcd1a501cf6eff4cbfa83
Agata Nowicka <nowicka.agata2201@gmail.com>
2020-02-20 23:51:44 +0100

    Add support mongoDB using mongoose.

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use('/static', express.static('public'));
 
-var allPosts=[];
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true })
+    .then(() => {
+        console.log('mongoose conected');
+    })
+    .catch((err) => {
+        console.log(`mongoose connection error ${err}`);
+    });
 
+const postSchema = mongoose.Schema({
+    image: String,
+    title: String,
+    text: String
+});
+const postModel = mongoose.model("post", postSchema);
+
+
+app.get("/home", function (req, res) {
+    postModel.find({}, function (err, foundPosts) {
+        console.log(JSON.stringify(foundPosts));
+        res.render("home", { allPosts: foundPosts });
+    })
+});
 app.get("/", function (req, res) {
-    console.log('get root');
-    res.render("home", {allPosts:allPosts});
+    res.render("amazingTravel");
 });
 
 app.get("/about", function (req, res) {
@@ -22,21 +42,52 @@ app.get("/contact", function (req, res) {
     res.render("contact");
 });
 
-app.get("/object", function (req, res) {
-    console.log('get object');
-    res.render("object");
+app.get("/postDetails/:postTitle", function (req, res) {
+    console.log( 'postTitle' + req.params.postTitle);
+    postModel.findOne({title: req.params.postTitle }, function (err, postToView) {
+        if (err) {
+            console.log(`error while processing`);
+            return res.redirect('/errorpage.html');
+        }
+        if (!postToView) {
+            console.log(`no post`);
+            return res.status(404).send();;
+        }
+        else {
+            res.render("postDetails", {
+                postTitle: postToView.title,
+                postText: postToView.text,
+                postImage: postToView.image
+            });
+        }
+    });
+})
+
+app.get("/postForm", function (req, res) {
+        res.render("postForm");
 });
 
-app.post("/object", function (req, res) {
-    console.log('post object');
-    var newPost = {
-        postTitle: req.body.title,
-        postText: req.body.contents
-    }
-    allPosts.push(newPost);
-    console.log(allPosts);
-    res.redirect('/');
-})
+app.post("/postForm", function (req, res) {
+    var post = new postModel({
+        title: req.body.title,
+        text: req.body.text,
+        image: req.body.image
+    });
+    post.save({}, function (err, savedPost) {
+        if (err) {
+            console.log(`error while processing`);
+            return res.redirect('/errorpage.html');
+        }
+        else {
+            console.log(`saved object with id: ${savedPost._id}`);
+            return res.redirect('/home');
+        }
+    });
+});
+
+app.get((req, res) => {
+     res.redirect('/errorpage.html');
+});
 
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
